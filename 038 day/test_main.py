64e6f14cc4af676a12026ce1b741704f8d7c3688
serb40 <serb40000@gmail.com>
2023-11-17 01:40:20 +0800

    38th day tests fixed to be PEP8 compliant

--- 038 day/test_main.py
+++ 038 day/test_main.py
@@ -6,8 +6,17 @@ from main import get_workouts, post_workout, format_workout, process_query
 class TestMain(unittest.TestCase):
     @patch("requests.get")
     def test_get_workouts(self, mock_get):
-        mock_response = {"workouts": [{"date": "01/01/2022", "time": "12:00:00", "exercise": "Running",
-                                       "duration": 30, "calories": 300}]}
+        mock_response = {
+            "workouts": [
+                {
+                    "date": "01/01/2022",
+                    "time": "12:00:00",
+                    "exercise": "Running",
+                    "duration": 30,
+                    "calories": 300
+                }
+            ]
+        }
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = mock_response
         response = get_workouts()
@@ -16,36 +25,77 @@ class TestMain(unittest.TestCase):
 
     @patch("requests.post")
     def test_post_workout(self, mock_post):
-        mock_workout = {"workout": {"date": "01/01/2022", "time": "12:00:00", "exercise": "Running",
-                                    "duration": 30, "calories": 300}}
+        mock_workout = {
+            "workout": {
+                "date": "01/01/2022",
+                "time": "12:00:00",
+                "exercise": "Running",
+                "duration": 30,
+                "calories": 300
+            }
+        }
         mock_post.return_value.status_code = 200
         mock_post.return_value.json.return_value = mock_workout
         response = post_workout(mock_workout)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), mock_workout)
+        self.assertEqual(response.status_code, 200,
+                         "Response status code should be 200")
+        self.assertEqual(response.json(), mock_workout,
+                         "Response JSON should match the mock workout")
 
     def test_format_workout(self):
         exercise = "Running"
         duration = 30
         calories = 300
-        expected_output = {"workout": {"date": "01/01/2022", "time": "12:00:00", "exercise": "Running",
-                                       "duration": 30, "calories": 300}}
+        expected_output = {
+            "workout": {
+                "date": "01/01/2022",
+                "time": "12:00:00",
+                "exercise": "Running",
+                "duration": 30,
+                "calories": 300
+            }
+        }
         with patch("main.dt") as mock_dt:
-            mock_dt.datetime.now.return_value.strftime.side_effect = ["01/01/2022", "12:00:00"]
+            mock_dt.datetime.now.return_value.strftime.side_effect = [
+                "01/01/2022", "12:00:00"
+            ]
             output = format_workout(exercise, duration, calories)
-            self.assertEqual(output, expected_output)
+            self.assertEqual(
+                output, expected_output,
+                "Output should match the expected workout format"
+            )
 
     @patch("requests.post")
     def test_process_query(self, mock_post):
-        mock_response = {"exercises": [{"name": "Running", "duration_min": 30, "nf_calories": 300}]}
+        mock_response = {
+            "exercises": [
+                {
+                    "name": "Running",
+                    "duration_min": 30,
+                    "nf_calories": 300,
+                },
+            ],
+        }
         mock_post.return_value.status_code = 200
         mock_post.return_value.json.return_value = mock_response
         query = "ran 3 miles"
-        headers = {"x-app-id": "test_id", "x-app-key": "test_key", "Content-Type": "application/json"}
-        parameters = {"query": query, "gender": "male", "weight_kg": 60, "height_cm": 180, "age": 35}
+        headers = {
+            "x-app-id": "test_id",
+            "x-app-key": "test_key",
+            "Content-Type": "application/json",
+        }
+        parameters = {
+            "query": query,
+            "gender": "male",
+            "weight_kg": 60,
+            "height_cm": 180,
+            "age": 35
+        }
         expected_output = [("Running", 30, 300)]
         with patch("main.dt") as mock_dt:
-            mock_dt.datetime.now.return_value.strftime.side_effect = ["01/01/2022", "12:00:00"]
+            mock_dt.datetime.now.return_value.strftime.side_effect = [
+                "01/01/2022", "12:00:00"
+            ]
             output = process_query(query, headers, parameters)
             self.assertEqual(output, expected_output)
 
