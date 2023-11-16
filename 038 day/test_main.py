2fe17027f5665db0b117678a348e080e5e894e8a
serb40 <serb40000@gmail.com>
2023-11-17 02:08:48 +0800

    38th day completed: tests in pytest

--- 038 day/test_main.py
+++ 038 day/test_main.py
@@ -1,104 +1,57 @@
-import unittest
-from unittest.mock import patch
+import pytest
+import requests_mock
+import os
 from main import get_workouts, post_workout, format_workout, process_query
 
+NUTRTIONIX_APP_ID = os.environ.get("NUTRITIONIX_APP_ID")
+NUTRTIONIX_API_KEY = os.environ.get("NUTRITIONIX_APP_KEY")
+NUTRTIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEETY_ENDPOINT_GET = os.environ.get("SHEETY_ENDPOINT_GET")
+SHEETY_ENDPOINT_POST = os.environ.get("SHEETY_ENDPOINT_POST")
+SHEETY_TOKEN = os.environ.get("SHEETY_TOKEN")
 
-class TestMain(unittest.TestCase):
-    @patch("requests.get")
-    def test_get_workouts(self, mock_get):
-        mock_response = {
-            "workouts": [
-                {
-                    "date": "01/01/2022",
-                    "time": "12:00:00",
-                    "exercise": "Running",
-                    "duration": 30,
-                    "calories": 300
-                }
-            ]
-        }
-        mock_get.return_value.status_code = 200
-        mock_get.return_value.json.return_value = mock_response
+
+# Mock environment variables
+@pytest.fixture(autouse=True)
+def mock_env_vars(monkeypatch):
+    monkeypatch.setenv("NUTRITIONIX_APP_ID", NUTRTIONIX_APP_ID)
+    monkeypatch.setenv("NUTRITIONIX_APP_KEY", NUTRTIONIX_API_KEY)
+    monkeypatch.setenv("SHEETY_ENDPOINT_GET", SHEETY_ENDPOINT_GET)
+    monkeypatch.setenv("SHEETY_ENDPOINT_POST", SHEETY_ENDPOINT_POST)
+    monkeypatch.setenv("SHEETY_TOKEN", SHEETY_TOKEN)
+
+
+# Test get_workouts function
+def test_get_workouts():
+    with requests_mock.Mocker() as m:
+        m.get(SHEETY_ENDPOINT_GET, json={"workouts": []})
         response = get_workouts()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), mock_response)
-
-    @patch("requests.post")
-    def test_post_workout(self, mock_post):
-        mock_workout = {
-            "workout": {
-                "date": "01/01/2022",
-                "time": "12:00:00",
-                "exercise": "Running",
-                "duration": 30,
-                "calories": 300
-            }
-        }
-        mock_post.return_value.status_code = 200
-        mock_post.return_value.json.return_value = mock_workout
-        response = post_workout(mock_workout)
-        self.assertEqual(response.status_code, 200,
-                         "Response status code should be 200")
-        self.assertEqual(response.json(), mock_workout,
-                         "Response JSON should match the mock workout")
-
-    def test_format_workout(self):
-        exercise = "Running"
-        duration = 30
-        calories = 300
-        expected_output = {
-            "workout": {
-                "date": "01/01/2022",
-                "time": "12:00:00",
-                "exercise": "Running",
-                "duration": 30,
-                "calories": 300
-            }
-        }
-        with patch("main.dt") as mock_dt:
-            mock_dt.datetime.now.return_value.strftime.side_effect = [
-                "01/01/2022", "12:00:00"
-            ]
-            output = format_workout(exercise, duration, calories)
-            self.assertEqual(
-                output, expected_output,
-                "Output should match the expected workout format"
-            )
-
-    @patch("requests.post")
-    def test_process_query(self, mock_post):
-        mock_response = {
-            "exercises": [
-                {
-                    "name": "Running",
-                    "duration_min": 30,
-                    "nf_calories": 300,
-                },
-            ],
-        }
-        mock_post.return_value.status_code = 200
-        mock_post.return_value.json.return_value = mock_response
-        query = "ran 3 miles"
-        headers = {
-            "x-app-id": "test_id",
-            "x-app-key": "test_key",
-            "Content-Type": "application/json",
-        }
-        parameters = {
-            "query": query,
-            "gender": "male",
-            "weight_kg": 60,
-            "height_cm": 180,
-            "age": 35
-        }
-        expected_output = [("Running", 30, 300)]
-        with patch("main.dt") as mock_dt:
-            mock_dt.datetime.now.return_value.strftime.side_effect = [
-                "01/01/2022", "12:00:00"
-            ]
-            output = process_query(query, headers, parameters)
-            self.assertEqual(output, expected_output)
+        assert response.status_code == 200
+        assert response.json() == {"workouts": []}
 
 
-if __name__ == "__main__":
-    unittest.main()
+# Test post_workout function
+def test_post_workout():
+    with requests_mock.Mocker() as m:
+        m.post(SHEETY_ENDPOINT_POST, json={"workout": {"id": 1}}, status_code=201)
+        response = post_workout({"workout": {}})
+        assert response.status_code == 201
+
+
+# Test format_workout function
+def test_format_workout():
+    workout = format_workout("Running", 30, 300)
+    assert workout["workout"]["exercise"] == "Running"
+    assert workout["workout"]["duration"] == 30
+    assert workout["workout"]["calories"] == 300
+
+
+# Test process_query function
+def test_process_query():
+    with requests_mock.Mocker() as m:
+        m.post(NUTRTIONIX_ENDPOINT, json={"exercises": [{"name": "running", "duration_min": 30, "nf_calories": 300}]})
+        results = process_query("ran 3 miles", {"Content-Type": "application/json"}, {"query": "ran 3 miles"})
+        assert len(results) == 1
+        assert results[0][0] == "Running"
+        assert results[0][1] == 30
+        assert results[0][2] == 300
